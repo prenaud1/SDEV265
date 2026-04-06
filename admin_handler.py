195141b63dd68496366ef6a7fe67d5ebdacde21f
AndriiSaveliev <150327111+AndriiSaveliev@users.noreply.github.com>
2026-04-06 01:21:55 -0400

    Add AdminHandler class for admin operations

---
+++ admin_handler.py
@@ -0,0 +1,20 @@
+class AdminHandler:
+    """
+    Handles admin operations.
+    """
+
+    def change_price(self, slot, price):
+        """Change product price"""
+        pass
+
+    def disable_slot(self, slot):
+        """Disable slot"""
+        pass
+
+    def enable_slot(self, slot):
+        """Enable slot"""
+        pass
+
+    def generate_report(self):
+        """Generate reports"""
+        pass
