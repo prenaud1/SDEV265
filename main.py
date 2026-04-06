44a1e25341e8855657f5658ba2f9ab93b1986a78
AndriiSaveliev <150327111+AndriiSaveliev@users.noreply.github.com>
2026-04-06 01:22:56 -0400

    Add console UI function for vending machine
    Implement basic console UI for vending machine.

---
+++ main.py
@@ -0,0 +1,24 @@
+from vending_machine import VendingMachine
+from product_handler import ProductHandler
+from money_handler import MoneyHandler
+from admin_handler import AdminHandler
+
+def console_ui():
+    """
+    Using the console, displays menus and prompts for input.
+    """
+    print("Vending Machine")
+    print("---------------")
+
+
+    print("Goodbye.")
+
+def main():
+    """
+    Basic UI (will be implemented later)
+    """
+    console_ui()
+
+
+if __name__ == "__main__":
+    main()
