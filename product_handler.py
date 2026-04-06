47db79e8fd9375a9a8cc9944edd44f60936bf7ff
AndriiSaveliev <150327111+AndriiSaveliev@users.noreply.github.com>
2026-04-06 01:24:26 -0400

    Add ProductHandler class for product management

---
+++ product_handler.py
@@ -0,0 +1,16 @@
+class ProductHandler:
+    """
+    Handles products and inventory.
+    """
+
+    def load_products(self):
+        """Load products from file"""
+        pass
+
+    def get_product(self, slot):
+        """Get product by slot"""
+        pass
+
+    def vend_product(self, slot):
+        """Dispense product"""
+        pass
