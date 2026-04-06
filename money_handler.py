4f685a06e463b85a706f352989275acf3c32664a
AndriiSaveliev <150327111+AndriiSaveliev@users.noreply.github.com>
2026-04-06 01:23:45 -0400

    Add MoneyHandler class for money operations

---
+++ money_handler.py
@@ -0,0 +1,20 @@
+class MoneyHandler:
+    """
+    Handles all money operations.
+    """
+
+    def insert_coin(self, coin):
+        """Add coin to balance"""
+        pass
+
+    def get_balance(self):
+        """Return current balance"""
+        pass
+
+    def make_change(self, amount):
+        """Return change"""
+        pass
+
+    def return_coins(self):
+        """Return all inserted coins"""
+        pass
